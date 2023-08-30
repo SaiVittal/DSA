ec6f2bf20578337591f68f5e428c401b735c22c6
Sai Vittal <sai.sirimamidi@zelarsoft.com>
2023-08-30 12:49:21 +0530

    third commit

---
+++ DS/Linked Lists/LinkedList.java
@@ -0,0 +1,12 @@
+
+
+/*Linked list is a data structure which a chain of Nodes, where node holds the data and the next
+nodes's address.
+
+IMPORTANT:
+1. INSERTION OF A NODE at Start, end, ith Position
+2. DELETION OF A NODE at  Start, end, ith Position
+3. REVERSE LL using recursively and iteratively
+4. Is LL PALINDROME
+5. Is LL CYCLIC
+*/
